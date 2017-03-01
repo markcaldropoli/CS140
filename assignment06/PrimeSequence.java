391701a1cf0089e3bd3a3d061a433a53d7b39ccf
Mark Caldropoli <mcaldro1@binghamton.edu>
2017-03-01 16:06:58 -0500

    Create PrimeSequence.java

---
+++ assignment06/PrimeSequence.java
@@ -0,0 +1,21 @@
+package assignment06;
+
+public class PrimeSequence implements Sequence {
+	private int n = 1;
+	
+	public int next() {
+		do {
+		    n += 1;
+		} while (!isPrime(n));
+		return n;
+	}
+	
+	public boolean isPrime(int x) {
+		if(x > 2 && (x & 1) == 0)
+		       return false;
+		    for(int i = 3; i * i <= x; i += 2)
+		        if (x % i == 0) 
+		            return false;
+		    return true;
+	}
+}
