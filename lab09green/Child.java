735ae9478af54707af9ed784b739546dd24c0fd3
Mark Caldropoli <mcaldro1@binghamton.edu>
2017-03-23 19:30:22 -0400

    Create Child.java

---
+++ lab09green/Child.java
@@ -0,0 +1,20 @@
+package lab09green;
+
+public class Child extends Parent {
+	private int[] array;
+	
+	public Child(int[] arr1, int[] arr2) {
+		super(arr1);
+		this.array = arr2;
+	}
+	
+	@Override
+	public double average() {
+		return new Parent(array).average();
+	}
+	
+	@Override
+	public int min() {
+		return new Parent(array).min();
+	}
+}
